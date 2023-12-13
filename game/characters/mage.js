7f275201c550391b09a4b270102a45ffa87b6426
Mathew McLeod <matymcleod@gmail.com>
2023-12-12 20:53:17 -0800

    adding knife class to mage

--- game/characters/mage.js
+++ game/characters/mage.js
@@ -1,10 +1,13 @@
 const Character = require("./character");
-const Lag = require("../spells/lag")
+const Lag = require("../spells/lag");
+const Knife = require("../weapons/knife");
+
 class Mage extends Character {
   constructor(name) {
     super(name, "Mage", 2, 4, 6,  8, 10, 12);
     this.name = name;
     this.spell = Lag;
+    this.weapon = Knife;
   }
 }
 
--- game/game.js
+++ game/game.js
@@ -3,14 +3,14 @@ const Warlock = require("./characters/warlock");
 const Mage = require("./characters/mage");
 const Spell = require("./spells/spell");
 
-const shaman = new Shaman("shaman");
-console.log('TEST::: shaman :::', shaman);
+// const shaman = new Shaman("shaman");
+// console.log('TEST::: shaman :::', shaman);
 
-const warlock = new Warlock("warlock");
-console.log('TEST::: warlock :::', warlock);
+// const warlock = new Warlock("warlock");
+// console.log('TEST::: warlock :::', warlock);
 
 const mage = new Mage("mage");
 console.log('TEST::: mage :::', mage);
 
-const spell = new Spell("test", 5, 6);
-console.log('TEST::: spell :::', spell);
+// const spell = new Spell("test", 5, 6);
+// console.log('TEST::: spell :::', spell);
