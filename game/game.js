198b5afd1f55735bc28414b06a608c0a50905ef9
Mathew McLeod <matymcleod@gmail.com>
2024-01-10 13:07:34 -0800

    adding basic game start logic to game loop, created mobs dir and added both mobs

--- game/game.js
+++ game/game.js
@@ -4,3 +4,28 @@ const Mage = require("./characters/mage");
 const Spell = require("./spells/spell");
 const prompt = require("prompt-promise");
 
+async function gameLoop() {
+  let character;
+  const classChoice = await prompt("Select a character: mage, shaman or warlock")
+  console.log(`You selected ${classChoice}`);
+  switch(classChoice) { 
+    case "mage":
+    character = new Mage("Player");
+    console.log(character);
+    break;
+
+    case "shaman":
+    character = new Shaman("Player");
+    console.log(character);
+    break;
+
+    case "warlock":
+    character = new Warlock("Player");
+    console.log(character);
+    break;
+    
+    default: console.log(`PLEASE CHOOSE A VALID CHARACTER(must be lower case)`);
+  }
+}
+
+gameLoop();
---
+++ game/mobs/mobs.js
@@ -0,0 +1,5 @@
+const badguy = require("./badguy");
+const buttface = require("./buttface");
+const mobs = [badguy, buttface];
+
+module.exports = mobs;
