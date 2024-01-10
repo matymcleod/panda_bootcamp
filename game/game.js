ff9fcef4f1cd9a3029a04d53298ddece1f2edbb4
Mathew McLeod <matymcleod@gmail.com>
2024-01-10 12:19:02 -0800

    added warlock tests

--- game/game.js
+++ game/game.js
@@ -5,20 +5,23 @@ const Spell = require("./spells/spell");
 const prompt = require("prompt-promise");
 
 // Mage tests
-// const mage = new Mage("mage");
-// console.log('TEST ((( mage DEFAULT )))', mage);
-// mage.levelUp();
-// console.log('TEST ((( mage.levelUp )))', mage.level);
-// mage.summonPet("dusty");
-// console.log('TEST ((( mage.summonPet )))', mage.activePet);
-// mage.equipWeapon("knife");
-// console.log('TEST ((( mage.equipWeapon )))', mage.equippedWeapon);
-// mage.getDamage();
-// console.log('TEST ((( mage.getDamage )))', mage);
+const mage = new Mage("mage");
+console.log('TEST ((( mage DEFAULT )))');
+console.log(mage);
+mage.levelUp();
+console.log('TEST ((( mage.levelUp )))', mage.level);
+mage.summonPet("dusty");
+console.log('TEST ((( mage.summonPet )))', mage.activePet);
+mage.equipWeapon("knife");
+console.log('TEST ((( mage.equipWeapon )))', mage.equippedWeapon);
+mage.getDamage();
+console.log('TEST ((( mage.getDamage )))');
+console.log(mage);
 
 // Shaman tests
 // const shaman = new Shaman("shaman");
-// console.log('TEST ((( shaman DEFAULT )))', shaman);
+// console.log('TEST ((( shaman DEFAULT )))');
+// console.log(shaman);
 // shaman.levelUp();
 // console.log('TEST ((( shaman.levelUp )))', shaman.level);
 // shaman.summonPet("sophie");
@@ -26,11 +29,13 @@ const prompt = require("prompt-promise");
 // shaman.equipWeapon("pistol");
 // console.log('TEST ((( shaman.equipWeapon )))', shaman.equippedWeapon);
 // shaman.getDamage("phase");
-// console.log('TEST ((( shaman.getDamage )))', shaman);
+// console.log('TEST ((( shaman.getDamage )))');
+// console.log(shaman);
 
 // Warlock tests
 // const warlock = new Warlock("warlock");
-// console.log('TEST ((( warlock DEFAULT )))', warlock);
+// console.log('TEST ((( warlock DEFAULT )))');
+// console.log(warlock);
 // warlock.levelUp();
 // console.log('TEST ((( warlock.levelUp )))', warlock.level);
 // warlock.summonPet("nikki");
@@ -38,4 +43,5 @@ const prompt = require("prompt-promise");
 // warlock.equipWeapon("sword");
 // console.log('TEST ((( warlock.equipWeapon )))', warlock.equippedWeapon);
 // warlock.getDamage("stun");
-// console.log('TEST ((( warlock.getDamage )))', warlock);
+// console.log('TEST ((( warlock.getDamage )))');
+// console.log(warlock);
