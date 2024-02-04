e6f27f424e2ddcb7d7c72bf1a9ae74e29a22e08c
Mathew McLeod <matymcleod@gmail.com>
2024-02-04 13:17:51 -0800

    More next mob logic and error handling

--- game/game.js
+++ game/game.js
@@ -11,7 +11,7 @@ async function gameLoop() {
   let mob = mobs[0];
 
   const classChoice = await prompt("Select a character:\n 1 = mage\n 2 = Shaman\n 3 = warlock\n")
-
+  const classChoiceErr = "PLEASE CHOOSE A VALID CHARACTER. \nGame Over. \nPress 'control' + 'D' to exit the game.";
   switch(classChoice) { 
     case "1":
       character = new Mage("MAGE");
@@ -23,13 +23,14 @@ async function gameLoop() {
       character = new Warlock("WARLOCK");
       break;
     default: 
-      console.log(`PLEASE CHOOSE A VALID CHARACTER(must be lower case). \nGame Over. \nPress 'control' + 'D' to exit the game.`);
+      console.log(`${classChoiceErr}`);
   }
       
-  console.log(`You have selected ((( ${character.name} ))) \nHealth = ${character.health}\nStrength = ${character.getDamage("lag")}\nLevel = ${character.level}`);
+  console.log(`You have selected ((( ${character.name} ))) \nHealth = ${character.health}\nLevel = ${character.level}`);
   
   const mobChoice = await prompt(`Select a Mob to fight:\n 1 = ${mobs[0].name}\n 2 = ${mobs[1].name}\n 3 = ${mobs[2].name}\n`)
 
+  const mobChoiceErr = "Please choose a valid Mob yo"
   switch(mobChoice) {
     case "1":
       mob = mobs[0];
@@ -41,55 +42,70 @@ async function gameLoop() {
       mob = mobs[2];
       break;
     default: 
-      console.log("Please choose either 1, 2 or 3 for valid mob")
+      console.log(`${mobChoiceErr}`)
   }
   console.log("You are now fighting <<< " + mob.name + " >>>\n Health = " + mob.health + "\n Attack = " + mob.attack);
-
-  const initAttack = await prompt("Begin the game by choosing to \n 1 = cast a spell\n 2 = use weapon\n 3 = attack with pet\n")
-
-  switch(initAttack) {
-    case "1":
-      const initSpell = await prompt(`You have chosen to cast a spell, which spell would you like to use?`)
-      console.log(character.spells);
-      break;
-    case "2":
-      const initWeapon = await prompt(`You have chose to attack with a Weapon, choose your weapon`)
-      console.log(character.weapons);
-      break;
-    case "3":
-      const initPet = await prompt(`You have chosen to attack with a Pet, choose your pet`)
-      console.log(character.pets);
-      break;
-    default: 
-      console.log("You entered something invalid, please quit and restart the game")
-    } 
-
-  while((character.health > 0) && (mob.health > 0)) {
-    // Character
-    const move = await prompt(`${character.name}!!!\nMake your next move\n`)
-    const damage = character.getDamage(move);
   
-    // Need to find a way to handle mob health stat when no input is entered in this prompt
-    console.log(`${character.name} HEALTH = ${character.health}`);
-
-    // Mob
-    mob.health -= damage;
-    console.log(`${mob.name} HEALTH = ${mob.health}`);
+  while((character.health > 0) && (mob.health > 0)) {
+    const charAttack = await prompt(`Your move ${character.name}\n 1 = cast a spell\n 2 = use weapon\n 3 = attack with pet\n`)
     
-    if(mob.health < 0){
-      console.log(`\nYAY! you defeated ${mob.name}`)
-      // need an array method to place the current mob at the begining of the mobs array 
-      // mobs.shift() // shift removes the first element of an array and changes the length of the array
-    }
-    const livingMobs = [];
-    for(let i = 0; i < mobs.length; i++) {
-      if(mobs[i].health > 0){
+    switch(charAttack) {
+      case "1":
+        console.log(character.spells);
+        const charSpell = await prompt(`You have chosen to cast a spell, which spell would you like to use?\n`);
+        const spellAttack = character.getDamage(charSpell);
+        mob.health -= spellAttack;
+        console.log(`You hit ${mob.name} for ${spellAttack}\n ${mob.name} Health = ${mob.health}`)
+        break;
         
-      }
+        case "2":
+          console.log(character.weapons);
+          const weaponAttack = await prompt(`You have chosen to attack with a Weapon, choose your weapon\n`)
+          break;
+          
+          case "3":
+            console.log(character.pets);
+            const attackPet = await prompt(`You have chosen to attack with a Pet, choose your pet\n`)
+            break;
+            
+      default: 
+        console.log("You entered something invalid, please quit and restart the game")
+      } 
+          
+      if(mob.health > 0){
+        // mob attack
+        character.health -= mob.attack
+        console.log(`${mob.name} hits you back for ${mob.attack}`);
+        console.log(`${character.name} Health = ${character.health}`)
+      } else {
+        console.log(`\nYAY! you defeated ${mob.name}`)
+        const livingMobs = [];
+        for(let i = 0; i < mobs.length; i++) {
+          let livingMob = mobs[i];
+          if(mobs[i].health > 0){
+            livingMobs.push(livingMob);
+          }
+        }
+        console.log(livingMobs);
+        const nextMob = await prompt(`Please choose the next mob to fight\n 1 = ${livingMobs[0]}\n 2 = ${livingMobs[1]}`)
+
+        switch(nextMob) {
+          case "1":
+            mob = livingMobs[0];
+            break;
+
+          case "2":
+            mob = livingMobs[1];
+            break;
+          
+          default:
+            console.log(`${mobChoiceErr}`)
+        }
+
     }
-    const nextMob = await prompt("Please choose the next mob to fight")
+
     // if all mobs have been beaten/removed from the mobs array, you win the game
-    if(.length < 1) {
+    if(livingMobs.length < 1) {
       return "Game Over, You Won!"
     }
   }
