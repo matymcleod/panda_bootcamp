dd7add8d4b895f2c4597f1249cfeb1a4d439c4c4
Mathew McLeod <matymcleod@gmail.com>
2024-02-09 21:33:24 -0800

    fixing switch statements indentation

--- game/game.js
+++ game/game.js
@@ -12,6 +12,8 @@ async function gameLoop() {
 
   const classChoice = await prompt("Select a character:\n 1 = mage\n 2 = Shaman\n 3 = warlock\n")
   const classChoiceErr = "PLEASE CHOOSE A VALID CHARACTER. \nGame Over. \nPress 'control' + 'D' to exit the game.";
+
+  // This switch allows users to create a new character based on which case number is input by the user.
   switch(classChoice) { 
     case "1":
       character = new Mage("MAGE");
@@ -25,7 +27,8 @@ async function gameLoop() {
     default: 
       console.log(`${classChoiceErr}`);
   }
-      
+  
+  // Once a charcter has been created, a message is displayed with the charcaters basic stats
   console.log(`You have selected ((( ${character.name} ))) \nHealth = ${character.health}\nLevel = ${character.level}`);
   
   const mobChoice = await prompt(`Select a Mob to fight:\n 1 = ${mobs[0].name}\n 2 = ${mobs[1].name}\n 3 = ${mobs[2].name}\n`)
@@ -57,37 +60,40 @@ async function gameLoop() {
         mob.health -= spellAttack;
         console.log(`You hit ${mob.name} for ${spellAttack}\n ${mob.name} Health = ${mob.health}`)
         break;
-        
-        case "2":
-          console.log(character.weapons);
-          const weaponAttack = await prompt(`You have chosen to attack with a Weapon, choose your weapon\n`)
-          break;
-          
-          case "3":
-            console.log(character.pets);
-            const attackPet = await prompt(`You have chosen to attack with a Pet, choose your pet\n`)
-            break;
-            
+      case "2":
+        console.log(character.weapons);
+        const weaponAttack = await prompt(`You have chosen to attack with a Weapon, choose your weapon\n`)
+        break;
+      case "3":
+        console.log(character.pets);
+        const attackPet = await prompt(`You have chosen to attack with a Pet, choose your pet\n`)
+        break;
       default: 
         console.log("You entered something invalid, please quit and restart the game")
-      } 
+    } 
           
+      const livingMobs = [];
       if(mob.health > 0){
+        
         // mob attack
         character.health -= mob.attack
         console.log(`${mob.name} hits you back for ${mob.attack}`);
         console.log(`${character.name} Health = ${character.health}`)
+        continue;
       } else {
         console.log(`\nYAY! you defeated ${mob.name}`)
-        const livingMobs = [];
         for(let i = 0; i < mobs.length; i++) {
           let livingMob = mobs[i];
           if(mobs[i].health > 0){
             livingMobs.push(livingMob);
+          } else {
+            // if all mobs have been beaten/removed from the mobs array, you win the game
+            return "Game Over, You Won!"
           }
         }
+        if(livingMobs.length > 1) {
         console.log(livingMobs);
-        const nextMob = await prompt(`Please choose the next mob to fight\n 1 = ${livingMobs[0]}\n 2 = ${livingMobs[1]}`)
+        const nextMob = await prompt(`Please choose the next mob to fight\n 1 = ${livingMobs[0].name}\n 2 = ${livingMobs[1].name}\n`)
 
         switch(nextMob) {
           case "1":
@@ -101,13 +107,13 @@ async function gameLoop() {
           default:
             console.log(`${mobChoiceErr}`)
         }
-
-    }
-
-    // if all mobs have been beaten/removed from the mobs array, you win the game
-    if(livingMobs.length < 1) {
-      return "Game Over, You Won!"
-    }
+      
+      } else {
+        mob = livingMobs[0];
+        console.log(`You are now fighting the final Mob boss ${mob}, good luck!`)
+        continue;
+      }
+      }
   }
 }
 gameLoop();
